a936e8520a840470f93a4c47ca2db7062a1e43fe
rdyjun <rdyjun00@gmail.com>
2023-07-03 15:59:04 +0900

    add Swagger class

--- src/main/java/dmucs/dmu/classroom/controller/OpenClassReservationController.java
+++ src/main/java/dmucs/dmu/classroom/controller/OpenClassReservationController.java
@@ -2,10 +2,10 @@ package dmucs.dmu.classroom.controller;
 
 import dmucs.dmu.classroom.dto.OpenClassRoomReservationDTO;
 import dmucs.dmu.classroom.service.OpenClassRoomReservationService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
@@ -14,10 +14,9 @@ import java.text.ParseException;
 @RequiredArgsConstructor
 public class OpenClassReservationController {
     private final OpenClassRoomReservationService classRoomReservationService;
-    @PostMapping
-    @RequestMapping("/reservation-room")
+    @ApiOperation(value = "공개 강의실 대여", notes = "공개 강의실 대여에 대한 정보 저장")
+    @PostMapping("/reservation-room")
     public void saveClassReservation (@RequestBody OpenClassRoomReservationDTO classRoomReservationDTO) throws ParseException {
         classRoomReservationService.reservationToRoom(classRoomReservationDTO);
-        System.out.println("성공!");
     }
 }
